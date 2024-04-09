b78ed9cbc9e3572ee04b12808e59f44774b54ae1
AJ Cooper <acooper@lcs.on.ca>
2024-04-09 11:55:14 -0400

    Put landing view into its own file

---
+++ ToDoList/Views/ItemView.swift
@@ -0,0 +1,18 @@
+//
+//  ItemView.swift
+//  ToDoList
+//
+//  Created by  Adam-James  Cooper on 2024-04-09.
+//
+
+import SwiftUI
+
+struct ItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemView()
+}
