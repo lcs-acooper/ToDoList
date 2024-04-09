76d2c3630d70e9619ea2a983c7156df6d794f19e
AJ Cooper <acooper@lcs.on.ca>
2024-04-09 11:08:37 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by  Adam-James  Cooper on 2024-04-09.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
